96c3f826812c70e0627106d71f6ce3a66fbea4f1
scasasr <CORREO@example.com>
2023-04-08 09:09:53 -0500

    to adjust home page title

--- nutricionapp/src/layouts/Main.js
+++ nutricionapp/src/layouts/Main.js
@@ -1,10 +1,86 @@
 import React from "react";
+
 import NavbarAll from "../components/Navbar.js";
+
+//React-Bootstrap-components
+import Carousel from 'react-bootstrap/Carousel';
+
+//Images
+import logo from "../assets/logo.png"
+
+//Icons
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+
 
 const Main = () => {
     return (  <>
         <NavbarAll/>
-        <h1>Main</h1>
+        <div className="principal-container">
+            <div className="principal d-flex justify-content-evenly">
+                <div>
+                    <h1 className="title">
+                        NutricionApp
+                    </h1>
+                    <h1 className="title-secondary">tu mejor aliado<br/>para una vida<br/>más saludable</h1>
+                </div>
+                <div>
+                    <img
+                        alt="logo"
+                        src={logo}
+                        width="400"
+                        height="320"
+                    />
+                </div>
+            </div>
+        </div>
+        <div className="container-white d-flex justify-content-around align-items-center">
+            <AccessTimeIcon sx={{ fontSize: 90, color:"#198754" }}/>
+            <p style={{color:"#fcba03"}}><b>Crea rutinas alimenticias y  llega a tus<br/>objetivos</b></p>
+            <EmojiEventsIcon sx={{ fontSize: 90, color:"#198754" }}/>
+            <p style={{color:"#fcba03"}}><b>Recetas saludables y deliciosas, pensadas<br/>en una vida mas saludable</b></p>
+            <MonitorHeartIcon sx={{ fontSize: 90, color:"#198754" }}/>
+        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     </>);
 }
  
